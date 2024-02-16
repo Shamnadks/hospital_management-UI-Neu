47cf5bc1812574e465c2f14fb61f392a1acf2bf0
Arun-k1998 <arunkottakkal98@gmail.com>
2024-02-16 21:13:20 +0530

    patient list page

--- app/src/app/n-services/service-caller.service.ts
+++ app/src/app/n-services/service-caller.service.ts
@@ -3,13 +3,15 @@
 
 import * as sd_3kUC36V9urSLMt09 from 'app/sd-services/addDoctorComponent'; //_splitter_
 import * as sd_WHTCUYDcvN50YSU7 from 'app/sd-services/appoinment'; //_splitter_
+import * as sd_06crRHWu4Z9W0XWR from 'app/sd-services/patient'; //_splitter_
 import { Injectable } from '@angular/core'; //_splitter_
 //append_imports_end
 @Injectable({ providedIn: 'root' })
 export class NeuServiceInvokerService {
   constructor(
     private sd_3kUC36V9urSLMt09: sd_3kUC36V9urSLMt09.addDoctorComponent,
-    private sd_WHTCUYDcvN50YSU7: sd_WHTCUYDcvN50YSU7.appoinment
+    private sd_WHTCUYDcvN50YSU7: sd_WHTCUYDcvN50YSU7.appoinment,
+    private sd_06crRHWu4Z9W0XWR: sd_06crRHWu4Z9W0XWR.patient
   ) {}
   invoke(
     injectedServiceId: string,
